aec5a1d13a776b440794a1348e3dfca4c0af17f8
timomak <tmakhlay2@gmail.com>
2022-11-25 15:47:24 -0500

    [FC-789] init: new component repo

---
+++ index.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import ChevronRightIcon from './assets/chevron-right.svg';
+import { View, Text, StyleSheet, TouchableOpacity, ViewStyle, TextStyle } from 'react-native';
+
+export interface PagingProps {
+    pagingConfig: { limit: number, skip: number, total: number };
+    onPageSelected: (page: number) => void;
+    current?: number;
+    onNextPressed: () => void;
+    onPrevPressed: () => void;
+    containerStyle?: ViewStyle;
+    itemStyle?: ViewStyle;
+    labelStyle?: TextStyle;
+    activeColor?: string;
+    mainColor?: string;
+}
+
+const createStyles = () =>
+    StyleSheet.create({
+        dotsContainer: {
+            flexDirection: 'row',
+            justifyContent: 'center'
+        },
+        dotContainer: {
+            borderWidth: 1,
+            width: 30,
+            height: 30,
+            borderRadius: 15,
+            alignContent: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'transparent',
+            alignSelf: 'center',
+            marginHorizontal: 5,
+            borderColor: 'transparent',
+        },
+        dotLabel: {
+            color: 'white   '
+        },
+        chevronContainer: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }
+    });
+
+export function PagingComponent({pagingConfig, onPageSelected, current = 0, onNextPressed, onPrevPressed, containerStyle, itemStyle, labelStyle, activeColor = 'red', mainColor = 'white'}: PagingProps) {
+    const styles = createStyles();
+
+    const moveToPage = (num: number) => {
+        onPageSelected(num);
+        onNextPressed();
+    };
+
+    const onNext = () => {
+        current + 1 === pagingConfig.total ? onPageSelected(pagingConfig.total - 1) : onPageSelected(current + 1);
+        onNextPressed();
+    };
+
+    const onPrev = () => {
+        current - 1 === -1 ? onPageSelected(0) : onPageSelected(current - 1);
+        onPrevPressed();
+    };
+
+    const RenderDot = (num: number) => {
+        const containerBorderColor = num === current ? activeColor : 'transparent';
+        return (
+            <TouchableOpacity onPress={() => onPageSelected(num)}>
+                <View style={[styles.dotContainer,  { borderColor: containerBorderColor }, itemStyle]}>
+                    <Text style={[styles.dotLabel,{ color: num === current ? activeColor : mainColor }, labelStyle]}>{num + 1}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    const RenderSeparator = (onPress: () => void) => {
+        return (
+            <TouchableOpacity onPress={onPress}>
+                <View style={[styles.dotContainer]}>
+                    <Text style={[styles.dotLabel, {color: mainColor}, labelStyle]}>. . .</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+
+    const renderDotNumbers = () => {
+        let dots = [];
+        // All Numbered Dots count can fit in the screen.
+        if (pagingConfig.total <= 6) {
+            for (let i = 0; i < pagingConfig.total; i++) {
+                dots.push(RenderDot(i));
+            }
+        }
+        // Show first or last 3 numbers are selected.
+        else if (current < 3 || current >= pagingConfig.total - 3) {
+            for (let i = 0; i < 3; i++) {
+                dots.push(RenderDot(i));
+            }
+            dots.push(RenderSeparator(current < 3 ? onNext : onPrev));
+            for (let i = 0; i < 3; i++) {
+                dots.push(RenderDot(pagingConfig.total - (3 - i)));
+            }
+        } else {
+            dots.push(RenderSeparator(() => moveToPage(0)));
+            [current - 2, current - 1, current, current + 1, current + 2].forEach((i) => dots.push(RenderDot(i)));
+            dots.push(RenderSeparator(() => moveToPage(pagingConfig.total - 1)));
+        }
+        return dots;
+    };
+
+    if (pagingConfig.total === 0) {
+        return null;
+    }
+
+    return (
+        <View style={[styles.dotsContainer, containerStyle]}>
+            <TouchableOpacity onPress={onPrev} style={[styles.chevronContainer]}>
+                <ChevronRightIcon style={[{ transform: [{scaleX: -1}] }]} stroke={mainColor} />
+            </TouchableOpacity>
+            {renderDotNumbers()}
+            <TouchableOpacity onPress={onNext} style={[styles.chevronContainer]}>
+                <ChevronRightIcon  stroke={mainColor}/>
+            </TouchableOpacity>
+        </View>
+    );
+}
